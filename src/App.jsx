5f1cf22b0da8107cc91198e82da9cb574302e67e
selenaho <selenaho2000@gmail.com>
2025-09-09 21:56:58 -0400

    added noun parsing using compromise library

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import nlp from 'compromise'
+
 function App() {
   const [text, setText] = useState("");
   const [displayedText, setDisplayedText] = useState("");
+  const [nouns, setNouns] = useState([]);
 
   return (
     <>
@@ -20,6 +23,9 @@ function App() {
         <br />
         <button
           onClick={() => {
+            const t = nlp(text);
+            setNouns(t.nouns().out('array'));
+            console.log(nouns); 
             setDisplayedText(text);
           }}
         >
@@ -30,6 +36,12 @@ function App() {
         <div className="displayed-text">
           <h2>Translation:</h2>
           <p>{displayedText}</p>
+          <h3>Nouns:</h3>
+          <ul>
+            {nouns.map((noun, index) => (
+              <li key={index}>{noun}</li>
+            ))}
+          </ul>
         </div>
       )}
     </>
