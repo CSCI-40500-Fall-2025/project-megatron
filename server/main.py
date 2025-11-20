efa9c36aea3901f047cf159c730b257b1be17beb
Jacky Wong <jackyw2500@gmail.com>
2025-11-19 21:26:08 -0500

    Combined logger.py into main.py

--- server/main.py
+++ server/main.py
@@ -3,11 +3,54 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 from dotenv import load_dotenv
 from typing import List, Optional
 from logger import setup_logging
 
 log = setup_logging(ci_mode=False)
+SUMO_ENDPOINT = os.getenv("SUMO_ENDPOINT")
+class SumoLogicHandler(logging.Handler):
+    def emit(self, record):
+        log_entry = self.format(record)
+        try:
+            requests.post(SUMO_ENDPOINT, data=log_entry.encode("utf-8"))
+        except Exception:
+            pass  # don't crash app on logging failure
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+def setup_logging(ci_mode=False):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG if ci_mode else logging.INFO)
+    logger.propagate = False
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if not ci_mode:
+        file_handler = RotatingFileHandler(
+            f"{LOG_DIR}/server.log",
+            maxBytes=5_000_000,
+            backupCount=5
+        )
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    
+    sumo_handler = SumoLogicHandler()
+    sumo_handler.setFormatter(formatter)
+    logger.addHandler(sumo_handler)
+    
+    return logger
 
 app = FastAPI()
 
